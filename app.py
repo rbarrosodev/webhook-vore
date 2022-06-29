8d7257d779a7ed8b32cb4909dc8dfe945f452b89
Rodrigo Barroso <digorbarroso@gmail.com>
2022-06-28 23:14:26 -0300

    test

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def webhook():
                 ingredients = req.get('queryResult')['queryText']
 
             return {
-                "fulfillmentText": f"""{'Então você é ' + fd_restrict.lower() if fd_restrict else 'Então você não tem restrições alimentares'}, {' tem até ' + time.lower() + ' para preparar receitas' if time else ' não tem preferência de tempo para receitas'}, {' quer preparar algo ' + taste.lower() if taste else ' não tem preferência por gosto específico'}, {'quer que renda ' + portions.lower() if portions else ' não tem preferência por tamanho de porção'}, {'quer auxílio de ' + celebrity_name if celebrity_name else ' não tem preferência por canal ou celebridade Globo'}, {' e têm ' + ingredients.lower() + ' na cozinha?' if ingredients else ' e não tem ingredientes adicionais?'}""",
+                "fulfillmentText": 'funcionou',
                 "source": 'webhook'
             }
         case 'restart':
