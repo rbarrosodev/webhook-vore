e143b74f86d2c946fd849462df185d388f37ef43
Rodrigo Barroso <digorbarroso@gmail.com>
2022-07-06 18:12:25 -0300

    uuid

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
     intent_name = req.get('queryResult')['intent']['displayName']
+    uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester'][
+        'uuid_user']
     if req.get('queryResult')['queryText'] == 'Recomeçar' or req.get('queryResult')['queryText'] == 'Voltar ao inicio':
         return {
             "followupEventInput": {
@@ -15,7 +17,16 @@ def webhook():
                 "languageCode": "en-US"
             }
         }
+        
     match intent_name:
+        case 'receita_duvida':
+            return {
+                "followupEventInput": {
+                    "name": "receita_duvida_teste",
+                    "languageCode": "en-US"
+                }
+            }
+
         case 'fallback':
             if '@' in req.get('queryResult')['queryText']:
                 return {
@@ -27,30 +38,35 @@ def webhook():
                     "fulfillmentText": 'Não entendi... Você pode repetir por favor?',
                     "source": 'webhook'
                 }
+
         case 'restrição_alimentar':
             global fd_restrict
             if req.get('queryResult')['queryText'] == "Não tenho":
                 fd_restrict = ""
             else:
                 fd_restrict = req.get('queryResult')['queryText']
+
         case 'tempo':
             global time
             if req.get('queryResult')['queryText'] == "Não precisa":
                 time = ""
             else:
                 time = req.get('queryResult')['queryText']
+
         case 'gosto':
             global taste
             if req.get('queryResult')['queryText'] == "Sem preferência":
                 taste = ""
             else:
                 taste = req.get('queryResult')['queryText']
+
         case 'celebridades':
             global celebrities
             if req.get('queryResult')['queryText'] == "Sem preferência":
                 celebrities = ""
             else:
                 celebrities = req.get('queryResult')['queryText']
+
         case 'ingredientes':
             global ingredients
             if req.get('queryResult')['queryText'] == "Não precisa":
@@ -62,6 +78,7 @@ def webhook():
                 "fulfillmentText": f"""{'Então você é ' + fd_restrict.lower() if fd_restrict else 'Então você não tem restrições alimentares'}, {' tem até ' + time.lower() + ' para preparar receitas' if time else ' não tem preferência de tempo para receitas'}, {' quer preparar algo ' + taste.lower() if taste else ' não tem preferência por gosto específico'}, {'quer auxílio de ' + celebrities if celebrities else ' não tem preferência por canal ou celebridade Globo'}, {' e têm ' + ingredients.lower() + ' na cozinha?' if ingredients else ' e não tem ingredientes adicionais?'}""",
                 "source": 'webhook'
             }
+
         case 'reiniciar':
             return {
                 "fulfillmentText": "Recomeçando...",
