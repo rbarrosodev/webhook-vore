2f65ae125d8f7d6e5b379f680715c2e1a41f4730
Rodrigo Barroso <digorbarroso@gmail.com>
2022-07-07 03:48:52 -0300

    pandas csv added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import json
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,8 +8,6 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
     intent_name = req.get('queryResult')['intent']['displayName']
-    uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester'][
-        'uuid_user']
     if req.get('queryResult')['queryText'] == 'Recomeçar' or req.get('queryResult')['queryText'] == 'Voltar ao inicio':
         return {
             "followupEventInput": {
@@ -40,44 +38,71 @@ def webhook():
                 }
 
         case 'restrição_alimentar':
-            global fd_restrict
-            if req.get('queryResult')['queryText'] == "Não tenho":
-                fd_restrict = ""
-            else:
-                fd_restrict = req.get('queryResult')['queryText']
+            df = pd.read_csv("data.csv",
+                             delimiter=';',
+                             skipinitialspace=True)
+            uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester']['uuid_user']
+            df2 = {'uuid': uuid}
+            if df[df['uuid'] == uuid].empty:
+                df = df.append(df2, ignore_index=True)
+                print(df)
+                df.to_csv('data.csv', index=False, sep=";")
+
+            if req.get('queryResult')['queryText'] != "Não tenho":
+                df = pd.read_csv("data.csv",
+                                 delimiter=';',
+                                 skipinitialspace=True)
+                df.at[df[df['uuid'] == uuid].index.values[0], 'fdr'] = req.get('queryResult')['queryText']
+                print(df)
+                df.to_csv('data.csv', index=False, sep=";")
+
 
         case 'tempo':
-            global time
-            if req.get('queryResult')['queryText'] == "Não precisa":
-                time = ""
-            else:
-                time = req.get('queryResult')['queryText']
+            if req.get('queryResult')['queryText'] != "Não precisa":
+                uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester']['uuid_user']
+                df = pd.read_csv("data.csv",
+                                 delimiter=';',
+                                 skipinitialspace=True)
+                df.at[df[df['uuid'] == uuid].index.values[0], 'time'] = req.get('queryResult')['queryText']
+                print(df)
+                df.to_csv('data.csv', index=False, sep=";")
 
         case 'gosto':
-            global taste
-            if req.get('queryResult')['queryText'] == "Sem preferência":
-                taste = ""
-            else:
-                taste = req.get('queryResult')['queryText']
+            if req.get('queryResult')['queryText'] != "Sem preferência":
+                uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester']['uuid_user']
+                df = pd.read_csv("data.csv",
+                                 delimiter=';',
+                                 skipinitialspace=True)
+                df.at[df[df['uuid'] == uuid].index.values[0], 'taste'] = req.get('queryResult')['queryText']
+                print(df)
+                df.to_csv('data.csv', index=False, sep=";")
 
         case 'celebridades':
-            global celebrities
-            if req.get('queryResult')['queryText'] == "Sem preferência":
-                celebrities = ""
-            else:
-                celebrities = req.get('queryResult')['queryText']
+            if req.get('queryResult')['queryText'] != "Sem preferência":
+                uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester']['uuid_user']
+                df = pd.read_csv("data.csv",
+                                 delimiter=';',
+                                 skipinitialspace=True)
+                df.at[df[df['uuid'] == uuid].index.values[0], 'celeb'] = req.get('queryResult')['queryText']
+                print(df)
+                df.to_csv('data.csv', index=False, sep=";")
 
         case 'ingredientes':
-            global ingredients
-            if req.get('queryResult')['queryText'] == "Não precisa":
-                ingredients = ""
-            else:
-                ingredients = req.get('queryResult')['queryText']
+            if req.get('queryResult')['queryText'] != "Não precisa":
+                uuid = req.get('originalDetectIntentRequest')['payload']['tiledesk']['payload']['request']['requester']['uuid_user']
+                df = pd.read_csv("data.csv",
+                                 delimiter=';',
+                                 skipinitialspace=True)
+                df.at[df[df['uuid'] == uuid].index.values[0], 'ing'] = req.get('queryResult')['queryText']
+                print(df)
+                df.to_csv('data.csv', index=False, sep=";")
 
-            return {
-                "fulfillmentText": f"""{'Então você é ' + fd_restrict.lower() if fd_restrict else 'Então você não tem restrições alimentares'}, {' tem até ' + time.lower() + ' para preparar receitas' if time else ' não tem preferência de tempo para receitas'}, {' quer preparar algo ' + taste.lower() if taste else ' não tem preferência por gosto específico'}, {'quer auxílio de ' + celebrities if celebrities else ' não tem preferência por canal ou celebridade Globo'}, {' e têm ' + ingredients.lower() + ' na cozinha?' if ingredients else ' e não tem ingredientes adicionais?'}""",
-                "source": 'webhook'
-            }
+                uindex = df[df['uuid'] == uuid].index.values[0]
+
+                return {
+                    "fulfillmentText": f"""{'Então você é ' + str(df.at[uindex, 'fdr']).lower() if str(df.at[uindex, 'fdr']) != 'nan' else 'Então você não tem restrições alimentares'}, {' tem até ' + str(df.at[uindex, 'time']).lower() + ' para preparar receitas' if str(df.at[uindex, 'time']) != 'nan' else ' não tem preferência de tempo para receitas'}, {' quer preparar algo ' + str(df.at[uindex, 'taste']).lower() if str(df.at[uindex, 'taste']) != 'nan' else ' não tem preferência por gosto específico'}, {'quer auxílio de ' + str(df.at[uindex, 'celeb']) if str(df.at[uindex, 'celeb']) != 'nan' else ' não tem preferência por canal ou celebridade Globo'}, {' e têm ' + str(df.at[uindex, 'ing']).lower() + ' na cozinha?' if str(df.at[uindex, 'ing']) != 'nan' else ' e não tem ingredientes adicionais?'}""",
+                    "source": 'webhook'
+                }
 
         case 'reiniciar':
             return {
